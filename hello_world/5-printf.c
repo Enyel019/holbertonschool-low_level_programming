a21bc1ab581fac9bdfe78788e06690ac9495f8b6
Enyel019 <enyelfm@gmail.com>
2022-07-19 02:13:04 +0000

    5. Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdo.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Sucess)
+  */
+
+
+int main(void)
+{
+	prinft("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

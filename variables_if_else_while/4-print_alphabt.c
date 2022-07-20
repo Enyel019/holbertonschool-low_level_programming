6d33e29d567e98bf1e17a27f1fc55753904485d1
Enyel019 <enyelfm@gmail.com>
2022-07-20 23:24:35 +0000

    4.When I was having that alphabet soup, I never thought that it would pay off

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main-hold all the code
+ * followed by a new line.
+ * Description:Print all the letters except q and e
+ * You can only use the putchar function (every other function
+ * (printf, puts, etc…) is forbidden)
+ * All your code should be in the main function
+ * You can only use putchar twice in your code
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	if (x == 'q' || i == 'e')
+	putchar(x);
+	putchar('\n');
+	return (0);
+}
+
+

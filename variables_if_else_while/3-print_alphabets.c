61cea604366135dbd2cfe8167e5f43a36ec4c1a3
Enyel019 <enyelfm@gmail.com>
2022-07-20 03:12:44 +0000

    3.alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main-hold all the code
+ * Description:Write a program that prints the alphabet in lowercase,
+ * followed by a new line
+ * You can only use the putchar function (every other function
+ * (printf, puts, etc…) is forbidden)
+ * All your code should be in the main function
+ * You can only use putchar twice in your code
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int l;
+	int a;
+
+	for (l = 97; l <= 122; l++)
+		putchar(l);
+	for (a = 65; a <= 90; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);

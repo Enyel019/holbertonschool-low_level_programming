2e59c6b1164d1596fe76c39d94be44e50aaf99ef
Enyel019 <enyelfm@gmail.com>
2022-07-21 02:26:51 +0000

    8.Hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -4,7 +4,7 @@
  * Description:Write a program that prints all the numbers
  * of base 16 in lowercase, followed by a new line.
  * You can only use the putchar function
- *(every other function (printf, puts, etc…) is forbidden)
+ * (every other function (printf, puts, etc…) is forbidden)
  * All your code should be in the main function
  * You can only use putchar three times in your code
  * Return: Always 0

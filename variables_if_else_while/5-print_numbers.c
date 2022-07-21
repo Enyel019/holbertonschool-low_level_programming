ffce2c304f769316a588ff27d67aceb624d3a6ec
Enyel019 <enyelfm@gmail.com>
2022-07-21 00:24:56 +0000

    5.Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main-hold all the code
+ * Description:Write a program that prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line.
+ * All your code should be in the main function
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
+

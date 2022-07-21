b1bb966b5d2d7df8ea16367d2fd274460a3a517b
Enyel019 <enyelfm@gmail.com>
2022-07-21 00:57:55 +0000

    6.Numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -13,12 +13,11 @@
 
 int main(void)
 {
-	int x;
-
-	for (x = 0; x <= 9; x++)
+int x;
+for (x = '0'; x <= '9'; x++)
 {
-	putchar(x);
+putchar(x);
 }
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }

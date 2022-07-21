f53ce5f037dfc3058b4897c2f9824d8d5345d4d8
Enyel019 <enyelfm@gmail.com>
2022-07-21 00:56:02 +0000

    6.Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main-hold all the code
+ * Description: Write a program that prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line.
+ * You are not allowed to use any variable of type char
+ * You can only use the putchar function (every other function
+ * (printf, puts, etc…) is forbidden)
+ * You can only use putchar twice in your code
+ * All your code should be in the main function
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+{
+	putchar(x);
+}
+	putchar('\n');
+	return (0);
+}

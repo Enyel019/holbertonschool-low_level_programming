ca055dc746c754af310442a46c1b05ffb8b4e6d4
Enyel019 <enyelfm@gmail.com>
2022-07-25 15:59:13 +0000

    0._putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- Hold all the code
+ * Description: Write a program that prints _putchar,
+ * followed by a new line.
+ * The program should return 0
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char enyel[] = "_putchar";
+	int x;
+
+	for (x = 0; x <= 9; x++)
+
+{
+	putchar(*(enyel + x));
+}
+	putchar('\n');
+	return (0);
+}
+

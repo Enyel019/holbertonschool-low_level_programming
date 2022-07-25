4ef36b900922ad009488b34377dfe6c3eb996b29
Enyel019 <enyelfm@gmail.com>
2022-07-25 19:10:38 +0000

    fix 1-main.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,7 +3,10 @@
 
 /**
  * print_alphabet - check the code
- * Description: 
+ * Description:Write a function that prints the alphabet,
+ * in lowercase, followed by a new line.
+ * Prototype: void print_alphabet(void);
+ * You can only use _putchar twice in your code
  * Return: Always 0.
  */
 

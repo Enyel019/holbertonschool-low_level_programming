742efd8b13cc65f2c5d09866f153a5c50055eb74
Enyel019 <enyelfm@gmail.com>
2022-07-25 19:01:38 +0000

    update

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - check the code
+ * Description: 
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+
+}

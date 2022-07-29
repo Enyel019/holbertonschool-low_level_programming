29752c9ed5e4a8c1cbfc1dfb91767d6e4a6fdfb2
Enyel019 <enyelfm@gmail.com>
2022-07-29 03:27:13 +0000

    0. Multiple mains

--- debugging/main.h
+++ debugging/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -10,9 +9,10 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
+
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */

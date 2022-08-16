d92a9ec5f871fcc0f9a594efba8c3edeaedef412
Enyel019 <enyelfm@gmail.com>
2022-08-16 02:35:28 +0000

    1.strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -16,11 +16,13 @@ char *_strncat(char *dest, char *src, int n)
 	int len2 = 0; /* string length for dest */
 	int i;
 
+	{
 	while (src[len1] != '\0')
 		len1++;
 
 	while (dest[len2] != '\0')
 		len2++;
+	}
 
 	for (i = 0; i < n; i++)
 	{

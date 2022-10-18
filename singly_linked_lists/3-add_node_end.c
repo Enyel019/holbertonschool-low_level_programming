cb9bd3f8ce8a88c82b01f6ace6abe8696727f581
Enyel019 <Enyelfm@gmail.com>
2022-10-18 00:56:10 +0000

    3.Add node at the end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -29,9 +29,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->str = b;
 	new_node->len = i;
 	new_node->next = NULL;
-
-	if(*head == NULL)
+	if (*head == NULL)
 	{
+
 		*head = new_node;
 		return (*head);
 	}

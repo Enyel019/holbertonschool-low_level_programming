ab10afaf03b63e04ee1fa99ce0463189c222c99b
Enyel019 <Enyelfm@gmail.com>
2022-10-24 23:35:04 +0000

    4.Free list

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint- function that frees a list
+ * @head: pointer to head of list
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *h;
+
+	while (head != NULL)
+	{
+		h = head;
+		head = head->next;
+		free(h);
+	}
+}

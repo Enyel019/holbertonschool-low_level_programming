dd2e0613a01f7ad518c2ad1f3e3cbeb089de577d
Enyel019 <Enyelfm@gmail.com>
2022-10-25 22:43:30 +0000

    6.Pop

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * pop_listint- delete the head node of a list and return the data
+ * @head: double pointer to head of list
+ *
+ * Return: head nodes data (n)
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *h;
+
+	if (*head == NULL)
+		return (0); /* condition */
+
+	if (head) /* if head exists */
+	{
+		h = *head;
+		n = h->n;
+		*head = h->next;
+		free(h);
+	}
+	return (n);
+}

7d10198a1124f02e5f88788b0439328147d9c020
Enyel019 <Enyelfm@gmail.com>
2022-11-03 00:29:56 +0000

    4.Free list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint- function that frees a list
+ * @head: pointer to head of list
+ *
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+
+{
+	dlistint_t *h;
+	while (head != NULL)
+	{
+		h = head;
+		head = head->next;
+		free(h);
+	}
+}
+
